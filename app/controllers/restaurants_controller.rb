2ae08ad4eebe41e2ec5c89141c7833e818ce6751
Ray De Leon <radeleon31@gmail.com>
2021-02-17 14:43:44 -0500

    all steps working

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,49 @@
+class RestaurantsController < ApplicationController
+  before_action :restaurant_find, only: [:edit, :update, :destroy]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @restaurant.update(restaurant_params)
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def destroy
+    @restaurant.destroy
+    redirect_to restaurants_path
+  end
+
+  private
+
+  def restaurant_find
+    @restaurant = restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    # Never trust user data!
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# "chinese", "italian", "japanese", "french", "belgian"
+
+puts "running info for restaurants"
+
+Restaurant.destroy_all
+
+Restaurant.create(name: "pepe", address: "palermo", category: "french", phone_number: "617-891-3457")
+Restaurant.create(name: "carbon", address: "NYC", category: "italian", phone_number: "618-893-3497")
+Restaurant.create(name: "Raku", address: "East Village", category: "japanese", phone_number: "212-871-3458")
+Restaurant.create(name: "Noodle Village", address: "China Town", category: "chinese", phone_number: "217-891-3457")
+Restaurant.create(name: "Waffle House", address: "Upper East", category: "belgian", phone_number: "248-781-3457")
+
+puts "done"
